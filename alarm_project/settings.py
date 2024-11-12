190f5f863757ac66b7716930f1689f4491b469fc
Himanshoo <thakrehimanshoo@gmail.com>
2024-11-12 23:11:22 +0530

    secret key

--- alarm_project/settings.py
+++ alarm_project/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-#django-insecure-=6%l0r(i=6&&x)(660jemz%7o(#vg5rhb2cf3zf)^@ixfo+jy^
+SECRET_KEY = os.environ.get('django-insecure-=6%l0r(i=6&&x)(660jemz%7o(#vg5rhb2cf3zf)^@ixfo+jy^')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower == 'true'
 
