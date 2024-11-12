9821d2895d997603832812e7f19473ca28d642e2
Himanshoo <thakrehimanshoo@gmail.com>
2024-11-12 22:57:57 +0530

    hosts

--- alarm_project/settings.py
+++ alarm_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower == 'true'
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'smartalarmclock-p8vr.onrender.com']
 
 
 # Application definition
